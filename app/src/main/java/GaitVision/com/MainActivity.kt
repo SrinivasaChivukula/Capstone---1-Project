bf120a829ebdd175c227f42036659c51cc97d338
Van Cung <122570520+vntrbl@users.noreply.github.com>
2024-10-11 16:59:41 -0500

    added buttons and activity switch
    app can now switch activity (screen) using the confirm video button

--- app/src/main/java/GaitVision/com/MainActivity.kt
+++ app/src/main/java/GaitVision/com/MainActivity.kt
@@ -1,21 +1,20 @@
 package GaitVision.com
 
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import GaitVision.com.ui.theme.GaitVisionTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val confirmVidBtn = findViewById<Button>(R.id.confirm_vid_btn)
+        confirmVidBtn.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/GaitVision/com/SecondActivity.kt
@@ -0,0 +1,11 @@
+package GaitVision.com
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
