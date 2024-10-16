1a73f665d33787ed454e93ca0e323908d4a4f473
Trevor <tspencergm1@gmail.com>
2024-10-15 20:07:40 -0500

    Update imageProcessing.kt
    Updated code within ProcessImage Bitmap to reflect a function throwing errors instead of null. Included debug output.

--- app/src/main/java/GaitVision/com/imageProcessing.kt
+++ app/src/main/java/GaitVision/com/imageProcessing.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.util.Log
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseLandmark
@@ -76,7 +77,7 @@ Name        : processImageBitmap
 Parameters  :
     context : This parameter is the interface that contains global information about
               the application environment.
-    bitmap  : This is the image bitmap we will use for processing.
+    bitmap  : This is the image bitmap we will use for processing. Can't be NULL
 Description : This function will take the context of the application and bitmap of the image/frame
               we will be processing. It then uses the ML Kit image processor to do pose detection on
               the image and get the pixel positions of specific points to be used for angle
@@ -91,34 +92,41 @@ fun processImageBitmap(context: Context, bitmap: Bitmap)
         .build()
     //Create instance of pose detector
     val poseDetector = PoseDetection.getClient(options)
-    val image = InputImage.fromBitmap(bitmap, 0)
-    if(image == null)
+
+    //Try-Catch statement because InputImage throws exception if there was an error creating the InputImage
+    try
     {
-        //display error with image bitmap.
+        val image = InputImage.fromBitmap(bitmap, 0)
+
+        poseDetector.process(image)
+            .addOnSuccessListener { pose ->
+                //Get all landmarks in image
+                //val allPoseLandMarks = pose.getAllPoseLandmarks() //Test case for all landmarks on image
+                //Get values for specific locations for calculations
+                val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+                val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
+                val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
+                val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+                val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
+                val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
+                val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
+                val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
+                val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
+                val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
+                val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
+                val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
+
+                Log.d("ErrorChecking","Landmarks detected")
+            }
+            .addOnFailureListener { e ->
+                Log.e("PoseDetection","Error detecting pose: ${e.message}")
+            }
+    }
+    catch (e: Exception)
+    {
+        Log.e("InputImage", "Error creating InputImage from bitmap: ${e.message}")
     }
 
-    poseDetector.process(image)
-        .addOnSuccessListener { pose ->
-            //Get all landmarks in image
-            //val allPoseLandMarks = pose.getAllPoseLandmarks() //Test case for all landmarks on image
-            //Get values for specific locations for calculations
-            val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
-            val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
-            val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
-            val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
-            val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
-            val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
-            val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
-            val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
-            val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
-            val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
-            val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
-            val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
-
-        }
-        .addOnFailureListener { e ->
-            println("Error detecting pose: ${e.message}")
-        }
 
 }
 
