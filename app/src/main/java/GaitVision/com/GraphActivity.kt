0c17458d30c3cb9b2e2b8613475b2f29b758d75e
Van Cung <122570520+vntrbl@users.noreply.github.com>
2024-10-30 20:43:12 -0500

    Update GraphActivity.kt

--- app/src/main/java/GaitVision/com/GraphActivity.kt
+++ app/src/main/java/GaitVision/com/GraphActivity.kt
@@ -1,20 +1,19 @@
 package GaitVision.com
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.content.Intent
+import androidx.activity.ComponentActivity
 
-class GraphActivity : AppCompatActivity() {
+class GraphActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_graph)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.graph_activity)
+
+        val uploadCSVBtn = findViewById<Button>(R.id.upload_csv_btn)
+        uploadCSVBtn.setOnClickListener {
+            val intent = Intent(this, UploadActivity::class.java)
+            startActivity(intent)
         }
     }
 }
