e35c109bef6e3910454c12162f2869fd7e8e8272
JoseBuen06 <158115835+JoseBuen06@users.noreply.github.com>
2024-12-04 21:21:53 -0600

    Add rename function
    Function is created to rename the edited video to include the participant ID.

--- app/src/main/java/GaitVision/com/ThirdActivity.kt
+++ app/src/main/java/GaitVision/com/ThirdActivity.kt
@@ -54,11 +54,12 @@ class ThirdActivity : ComponentActivity() {
                 }    //filename participantId_angle
 
                 writeToFile(fileName, fileData[i])                 //write to file is called with file name and placeholder as parameters
+                renameTo(participantId.text.toString())
             }
 
             val builder: AlertDialog.Builder = AlertDialog.Builder(this)
             builder
-                .setMessage("CSV Files saved to Documents as ParticipantID_GraphName.csv")
+                .setMessage("CSV Files saved to Documents as ParticipantID_GraphName.csv.\n\nUpdated video saved to Videos as ParticipantID_video.mp4")
                 .setTitle("Successfully Exported")
 
             val dialog: AlertDialog = builder.create()
@@ -143,5 +144,21 @@ class ThirdActivity : ComponentActivity() {
             }
 
         }
+
+
+    }
+
+    //Function for renaming the edited video
+    private fun renameTo(participantId:String) {
+        val vidName = buildString {     //string is built to include participant ID in the name
+            append("Movies/")
+            append(participantId)
+            append("_video.mp4")
+        }
+
+        val oldFilePath = File(Environment.getExternalStorageDirectory(), "Movies/edited_video.mp4")    //Path of the existing edited video
+        val newFilePath = File(Environment.getExternalStorageDirectory(), vidName)                           //New path for the renamed video
+
+        oldFilePath.renameTo(newFilePath)
     }
 }
