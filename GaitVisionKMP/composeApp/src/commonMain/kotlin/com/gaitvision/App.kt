c705556148d6845bd4fe45636c9f0ab939eec61b
SrinivasaChivukula <srinivasachivukula@outlook.com>
2025-12-08 13:43:26 -0600

    Clean up imports in App.kt
    Removed unused imports from App.kt

--- GaitVisionKMP/composeApp/src/commonMain/kotlin/com/gaitvision/App.kt
+++ GaitVisionKMP/composeApp/src/commonMain/kotlin/com/gaitvision/App.kt
@@ -1,19 +1,3 @@
-package com.gaitvision
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import com.gaitvision.ui.AppNavigation
-import com.gaitvision.ui.GaitVisionTheme
-
-import com.gaitvision.data.AppDatabase
-import com.gaitvision.platform.PoseDetector
-import com.gaitvision.platform.VideoProcessor
 
 @Composable
 fun App(
